3fa865308aead6858fef910bb222571196299201
tiagoriosrocha <tiagoriosrocha@gmail.com>
2021-08-03 15:23:05 -0300

    mensagem do comando help

--- app/Telegram/Commands/HelpCommand.php
+++ app/Telegram/Commands/HelpCommand.php
@@ -33,10 +33,16 @@ class HelpCommand extends Command
     {
         $response = $this->getUpdate();
         
-        $text = 'Hey stranger, thanks for visiting me.'.chr(10).chr(10);
-        $text .= 'I am a bot and working for'.chr(10);
-        $text .= env('APP_URL').chr(10).chr(10);
-        $text .= 'Please come and visit me there.'.chr(10);
+        //$text = 'Hey stranger, thanks for visiting me.'.chr(10).chr(10);
+        //$text .= 'I am a bot and working for'.chr(10);
+        //$text .= env('APP_URL').chr(10).chr(10);
+        //$text .= 'Please come and visit me there.'.chr(10);
+
+        $text = "Olá!".chr(10).chr(10);
+        $text.= "Seja bem vindo ao Bot - DayNotes!".chr(10).chr(10);
+        $text.= "Este bot tem a intenção de fazer o levantamento de como foi o seu dia a partir de algumas perguntas simples que você irá enumerar de 1 a 5.".chr(10).chr(10);
+        $text.= "As perguntas serão enviadas no final da noite, às 22h".chr(10).chr(10);
+        $text.= "Caso você tenha interesse de se inscrever, por favor, envie o comando /register".chr(10);
         
         $this->replyWithMessage(compact('text'));
 
