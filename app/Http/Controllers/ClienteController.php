579c130d1d971a77b7462cb323ad3fe24edce1f3
tiagoriosrocha <tiagoriosrocha@gmail.com>
2021-08-04 14:34:38 -0300

    registro e desregistro

--- app/Http/Controllers/ClienteController.php
+++ app/Http/Controllers/ClienteController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use Carbon\Carbon;
+use App\Models\Cliente;
 
 class ClienteController extends Controller
 {
